f94dc79a642a42f909ba449d80605af0cb6dc4a5
Manuel Hernández <manuel@secret-source.eu>
2021-01-31 18:52:30 +0000

    feat: added remove method to Binary Search Tree

--- data-structures/trees/binaryTree/index.test.ts
+++ data-structures/trees/binaryTree/index.test.ts
@@ -1,12 +1,18 @@
 import BinarySearchTree, { BinaryTreeNode } from './index';
 
-describe("BinarySearchTree", () => {
+function insertBatchValues(tree: BinarySearchTree, values: number[]): void {
+  for (let value of values) {
+    tree.insert(value);
+  }
+}
+
+describe('BinarySearchTree', () => {
   let rootNode: BinaryTreeNode;
   beforeEach(() => {
     rootNode = new BinaryTreeNode(30);
-  })
+  });
 
-  test("A new binary search tree can be created only when a valid root node is passed", () => {
+  test('A new binary search tree can be created only when a valid root node is passed', () => {
     let tree = new BinarySearchTree(rootNode);
     expect(tree.root).not.toBeNull();
     expect(tree.root.value).toEqual(30);
@@ -21,13 +27,13 @@ describe("BinarySearchTree", () => {
     expect(tree.root.value).toEqual(0);
   });
 
-  describe("Node insertion", () => {
+  describe('Node insertion', () => {
     let baseTree: BinarySearchTree;
     beforeEach(() => {
       baseTree = new BinarySearchTree(rootNode);
     });
 
-    test("Adds nodes correctly at both sides of the root", () => {
+    test('Adds nodes correctly at both sides of the root', () => {
       expect(baseTree.root.value).toEqual(30);
       expect(baseTree.root.left).toBeNull();
       expect(baseTree.root.right).toBeNull();
@@ -38,44 +44,52 @@ describe("BinarySearchTree", () => {
       expect(baseTree.root.right.value).toEqual(37);
     });
 
-    test("Adds nodes into a third level", () => {
+    test('Adds nodes into a third level', () => {
       expect(baseTree.root.value).toEqual(30);
 
       /**
        *        30
-       *    25       37    
+       *    25       37
        * 23   27   32  39
        */
       const valuesToInsert = [25, 37, 27, 32, 39, 23];
-      for (let value of valuesToInsert) {
-        baseTree.insert(value);
-      }
+      insertBatchValues(baseTree, valuesToInsert);
+      // for (let value of valuesToInsert) {
+      //   baseTree.insert(value);
+      // }
 
       const secondLevel = [baseTree.root.left, baseTree.root.right];
-      const secondLevelValues: number[] = secondLevel.map(node => node.value);
+      const secondLevelValues: number[] = secondLevel.map((node) => node.value);
 
-      const thirdLevel: BinaryTreeNode[][] =  secondLevel.map(node => [node.left, node.right]);
-      const thirdLevelValues: number[] = thirdLevel.reduce<number[]>((prev, current) => {
-        current.forEach(node => prev.push(node.value));
-        return prev;
-      }, []);
+      const thirdLevel: BinaryTreeNode[][] = secondLevel.map((node) => [
+        node.left,
+        node.right,
+      ]);
+      const thirdLevelValues: number[] = thirdLevel.reduce<number[]>(
+        (prev, current) => {
+          current.forEach((node) => prev.push(node.value));
+          return prev;
+        },
+        []
+      );
 
       expect(secondLevelValues).toEqual([25, 37]);
       expect(thirdLevelValues).toEqual([23, 27, 32, 39]);
-    })
+    });
   });
 
-  describe("Node lookup", () => {
-    let tree: BinarySearchTree; 
+  describe('Node lookup', () => {
+    let tree: BinarySearchTree;
     const treeValues = [25, 37, 27, 32, 39, 23];
     beforeEach(() => {
       tree = new BinarySearchTree(rootNode);
-      for (let value of treeValues) {
-        tree.insert(value);
-      }
+      insertBatchValues(tree, treeValues);
+      // for (let value of treeValues) {
+      //   tree.insert(value);
+      // }
     });
 
-    test("Can find a node with a value that is stored in the tree", () => {
+    test('Can find a node with a value that is stored in the tree', () => {
       const searchValues = [30, 32, 23];
       for (let searchValue of searchValues) {
         const lookupResult = tree.lookup(searchValue);
@@ -86,7 +100,7 @@ describe("BinarySearchTree", () => {
       }
     });
 
-    test("Returns null if the value cannot be found in the tree", () => {
+    test('Returns null if the value cannot be found in the tree', () => {
       const searchValues = [1, 28, 9, 15];
       for (let searchValue of searchValues) {
         const lookupResult = tree.lookup(searchValue);
@@ -94,4 +108,135 @@ describe("BinarySearchTree", () => {
       }
     });
   });
-})
+
+  describe('Check if a node is a leaf', () => {
+    let tree: BinarySearchTree;
+    const treeValues = [25, 37, 27, 32, 39, 23];
+    beforeEach(() => {
+      tree = new BinarySearchTree(rootNode);
+      insertBatchValues(tree, treeValues);
+    });
+
+    test('When a node is not a leaf must return false', () => {
+      expect(tree.isLeaf(tree.root)).toEqual(false);
+      expect(tree.isLeaf(tree.root.right)).toEqual(false);
+      expect(tree.isLeaf(tree.root.left)).toEqual(false);
+    });
+
+    test('When a node is a leaf must return true', () => {
+      const leafValues = [27, 23, 39, 32];
+
+      for (let value of leafValues) {
+        const node = tree.lookup(value);
+        expect(tree.isLeaf(node)).toEqual(true);
+      }
+    });
+  });
+
+  describe('GetSingleChild', () => {
+    let tree: BinarySearchTree;
+    const treeValues = [25, 37, 27, 32];
+    beforeEach(() => {
+      tree = new BinarySearchTree(rootNode);
+      insertBatchValues(tree, treeValues);
+    });
+
+    test('Returns null if a node has no children', () => {
+      const nodesWithouthChildren = [27, 32];
+      for (let value of nodesWithouthChildren) {
+        const node = tree.lookup(value);
+        expect(tree.getSingleChild(node)).toEqual(null);
+      }
+    });
+
+    test('Returns null if the node has more than one child', () => {
+      expect(tree.getSingleChild(tree.root)).toEqual(null);
+    });
+
+    test('Returns the single child of the given node', () => {
+      const nodesWithSingleChild = [25, 37];
+      for (let value of nodesWithSingleChild) {
+        const node = tree.lookup(value);
+        const child = tree.getSingleChild(node);
+        expect(child).not.toBeNull();
+      }
+    });
+  });
+
+  describe.skip('GetNodeSuccesor', () => {
+    let tree: BinarySearchTree;
+    const treeValues = [25, 37, 27, 32];
+    beforeEach(() => {
+      tree = new BinarySearchTree(rootNode);
+      insertBatchValues(tree, treeValues);
+    });
+
+    test('Returns the given node if it has no right child');
+    test('Returns the direct right child if that one has no left child');
+  });
+
+  describe.only('Remove a tree node', () => {
+    let tree: BinarySearchTree;
+    const treeValues = [25, 37, 27, 32, 39, 23];
+    beforeEach(() => {
+      tree = new BinarySearchTree(rootNode);
+      insertBatchValues(tree, treeValues);
+    });
+
+    test('Remove a node with no children', () => {
+      expect(tree.lookup(39)).toEqual({ left: null, right: null, value: 39 });
+      tree.remove(39);
+      expect(tree.lookup(39)).toEqual(null);
+    });
+
+    test('Remove a node with only right child', () => {
+      const expectedNode = new BinaryTreeNode(37);
+      expectedNode.right = new BinaryTreeNode(39);
+
+      tree.remove(32);
+      expect(tree.lookup(37)).toEqual(expectedNode);
+
+      tree.remove(37);
+      expect(tree.lookup(37)).toEqual(null);
+      expect(tree.lookup(30).right).toEqual({
+        value: 39,
+        left: null,
+        right: null,
+      });
+    });
+
+    test('Remove a node with only left child', () => {
+      // remove node 39 to leave node 37 with only a left child
+      tree.remove(39);
+      expect(tree.lookup(37)).toHaveProperty('left', {
+        left: null,
+        right: null,
+        value: 32,
+      });
+
+      tree.remove(37);
+      expect(tree.root.right).toHaveProperty('value', 32);
+    });
+
+    test('Remove a node with two children', () => {
+      expect(tree.root.right).toHaveProperty('value', 37);
+      expect(tree.root.right.right).toHaveProperty('value', 39);
+      expect(tree.root.right.left).toHaveProperty('value', 32);
+
+      tree.remove(37);
+      expect(tree.root.right).toHaveProperty('value', 39);
+      expect(tree.root.right).toHaveProperty('left', {
+        left: null,
+        right: null,
+        value: 32,
+      });
+    });
+
+    test('Remove a node with multiple children', () => {
+      tree.remove(30);
+      expect(tree.root).toHaveProperty('value', 32);
+      expect(tree.root.right).toHaveProperty('value', 37);
+      expect(tree.root.right.right).toHaveProperty('value', 39);
+    });
+  });
+});
--- data-structures/trees/binaryTree/index.ts
+++ data-structures/trees/binaryTree/index.ts
@@ -1,4 +1,4 @@
-import { TreeNode } from "../index"
+import { TreeNode } from '../index';
 
 export class BinaryTreeNode implements TreeNode {
   value: number;
@@ -23,7 +23,7 @@ export default class BinarySearchTree implements BinaryTree {
     if (!rootNode || rootNode.value == null) return;
     this.root = rootNode;
   }
-  
+
   /**
    * Inserts a new node inside the tree
    * Traverses the tree nodes to find the correct spot to place it
@@ -38,7 +38,7 @@ export default class BinarySearchTree implements BinaryTree {
     }
 
     // Traverse the nodes checking where the node should go
-    let current: BinaryTreeNode | null = this.root
+    let current: BinaryTreeNode | null = this.root;
     while (current) {
       if (value < current.value) {
         if (!current.left) {
@@ -71,11 +71,11 @@ export default class BinarySearchTree implements BinaryTree {
     let result: BinaryTreeNode | null = null;
     let current: BinaryTreeNode | null = this.root;
 
-    while(current) {
+    while (current) {
       if (current.value === value) {
         result = current;
         break;
-      } 
+      }
 
       if (value < current.value) {
         current = current.left;
@@ -86,4 +86,89 @@ export default class BinarySearchTree implements BinaryTree {
 
     return result;
   };
+
+  remove = (value: number): BinarySearchTree => {
+    if (!this.root) return null;
+
+    let parent = null;
+    let parentMove = null;
+    let current = this.root;
+    while (current) {
+      if (current.value === value) {
+        // current node is the target to remove
+
+        // check if the target is a leaf
+        if (this.isLeaf(current)) {
+          parent[parentMove] = null;
+          current = null;
+          return;
+        }
+
+        // check if the target has only one child
+        if (this.getSingleChild(current)) {
+          const successor = this.getSingleChild(current);
+          parent[parentMove] = successor;
+          current = null;
+          return;
+        }
+
+        // if target has two children replace with the right branch and left child
+        const successor = this.getNodeSuccessor(current);
+        if (current.value === this.root.value) {
+          this.root = successor;
+        }
+        successor.left = current.left;
+        successor.right = current.right;
+
+        if (parent && parentMove) {
+          parent[parentMove] = successor;
+        }
+        return;
+      }
+
+      if (value < current.value) {
+        parent = current;
+        parentMove = 'left';
+        current = current.left;
+      } else {
+        parent = current;
+        parentMove = 'right';
+        current = current.right;
+      }
+    }
+
+    return this.root;
+  };
+
+  isLeaf = (node: BinaryTreeNode): boolean => {
+    return !node.left && !node.right;
+  };
+
+  getSingleChild = (node: BinaryTreeNode): BinaryTreeNode | null => {
+    if (node.left && !node.right) {
+      return node.left;
+    }
+    if (node.right && !node.left) {
+      return node.right;
+    }
+
+    return null;
+  };
+
+  getNodeSuccessor = (node: BinaryTreeNode): BinaryTreeNode | null => {
+    if (!node) return null;
+    if (!node.right) return node;
+
+    const rightNode = node.right;
+    return rightNode.left || rightNode;
+  };
 }
+
+// const rootNode = new BinaryTreeNode(30);
+// let tree: BinarySearchTree;
+// const values = [25, 37, 27, 32, 39, 23];
+// tree = new BinarySearchTree(rootNode);
+// for (let value of values) {
+//   tree.insert(value);
+// }
+// tree.remove(39);
